fe7ae5f0714483a3fdf25c69390ffcf3266d0c0e
Motiur Rahman <codehiker07.com>
2024-06-08 07:53:24 +0600

    data save to LowDB in json format

---
+++ index4-save-data.mjs
@@ -0,0 +1,82 @@
+import puppeteer, { Page } from "puppeteer";
+import { setTimeout } from "timers/promises";
+
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+const db = new Low(new JSONFile("ecommerce.json"), {});
+await db.read();
+
+const saveToDB = async (id, prductData) => {
+  db.data[id] = prductData;
+  await db.write();
+};
+
+const browser = await puppeteer.launch({
+  headless: false,
+  userDataDir: "/tmp/ecommerce-crawler",
+});
+const page = await browser.newPage();
+
+await page.goto("https://www.studioneat.com", { waitUntil: "networkidle0" });
+await page.waitForSelector(".product-title a");
+const productLinks = await page.evaluate(() => {
+  return [...document.querySelectorAll(".product-title a")].map((e) => e.href);
+});
+
+console.log(productLinks);
+await page.close();
+// await browser.close()
+
+/**
+ * @param {Page} page
+ * @param {String} selector
+ */
+
+const extractText = (page, selector) => {
+  return page.evaluate((selector) => {
+    return document.querySelector(selector)?.innerHTML;
+  }, selector);
+};
+
+for (let productLink of productLinks) {
+  if (db.data[productLink]) {
+    console.log("Item already exists");
+    continue;
+  }
+  console.log(productLink);
+
+  const page = await browser.newPage();
+  await page.goto(productLink, { waitUntil: "networkidle0" });
+  await page.waitForSelector(".ecomm-container h1");
+
+  const title = await extractText(page, ".ecomm-container h1");
+  const tagline = await extractText(page, ".product-tagline");
+  const price = await extractText(page, "#productPrice");
+  const description = await extractText(page, ".product-desc");
+
+  const variants = await page.evaluate(() => {
+    return [...document.querySelectorAll(".single-option-selector option")].map(
+      (e) => e.value
+    );
+  });
+
+  const variantData = [];
+
+  for (const variant of variants) {
+    page.select(".single-option-selector", variant);
+    await setTimeout(100);
+    // await page.$eval('#productPrice', e=>e.innerHTML)
+    variantData.push({
+      variant,
+      price: await extractText(page, "#productPrice"),
+    });
+  }
+  await saveToDB(productLink, {
+    title,
+    tagline,
+    price,
+    description,
+    variantData,
+  });
+  await page.close();
+}
